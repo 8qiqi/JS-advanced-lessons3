c3c4c8a162fac1aaa550aa93f8bf05aecb0e905d
8qiqi <3510363360@qq.com>
2018-03-15 20:46:47 +0800

    LS02

---
+++ LS02/demo03.js
@@ -0,0 +1,42 @@
+//Number方法
+//ceil()向上取整  floor()向下取整  round()四舍五入
+var a2 = 23.4;
+console.log(Math.ceil(a2));//24
+console.log(Math.floor(a2));//23
+console.log(Math.round(a2));//23
+a3 = 5e2;//指数形式  等同于5e+2
+console.log(a3);//500
+console.log(typeof Math);//object 
+//toFixed(x)四舍五入小数点后取x位  toPrecision(x)转换成指数形式小数点后取x-1位
+//toString()转换成字符串格式       toExponential(2)转换成指数形式小数点后取x位
+var n1 = 12345.6789;
+console.log(n1.toFixed(2));//12345.68
+console.log(n1.toPrecision(2));//1.2e+4
+console.log(n1.toString());//12345.6789
+console.log(n1.toExponential(2));//1.23e+4
+
+//NaN
+var x = Number("xis021");//试着转成Number类型
+console.log(x);//NaN
+isNaN(x);//true
+typeof NaN;//"number"
+
+//acos()返回了一些弧度的反余弦值  log() 方法可返回一个数的自然对数
+console.log(Math.log(-1));//NaN
+console.log(Math.acos(2));//NaN 
+console.log(NaN === NaN);//false
+
+
+//Infinity与-Infinity
+var y1 = 2/0;
+console.log(y1);//Infinity
+var y2 = -2/0;
+console.log(y2);//-Infinity
+isFinite(y2);//false，非有限数
+isFinite(23);//true，有限数
+
+//0与-0
+var z1 = 1/Infinity;
+console.log(z1);//0
+var z2 = -1/Infinity;
+console.log(z2);//-0
---
+++ LS02/demo04.js
@@ -0,0 +1,39 @@
+//String方法
+var str="abc_def_ghi_jkl_mn";
+str.split("_");//["abc", "def", "ghi", "jkl", "mn"]
+str.split("_",2);// ["abc", "def"]
+str.concat("_qqq");//"abc_def_ghi_jkl_mn_qqq"
+str.substr(4,7);//"def_ghi"
+str.substring(4,7);//"def"
+str.slice(2,5);//"c_d"
+str.slice(2);//"c_def_ghi_jkl_mn"
+str.slice(2,-2);//"c_def_ghi_jkl_"
+str.slice(-2);//"mn"
+//split()分割  
+//substr()第几位开始截取几位
+//substring()第几位开始截取到第几位之前
+//slice()第几位开始截取到第几位之前
+//slice(start,end)和substring(start,end)
+
+//他们两个的end都是原字符串的索引，意思为截取到end（不包括end）位置的字符
+
+//二者的区别是：
+
+//slice中的start如果为负数，会从尾部算起，-1表示倒数第一个，-2表示倒数第2个，此时end必须为负数，并且是大于start的负数，否则返回空字符串
+var s="afghtee";
+s.slice(-1,3)//""
+//slice的end如果为负数，同样从尾部算起，如果其绝对值超过原字符串长度或者为0，返回空字符串
+var s="abcd";
+s.slice(2,-5);//""
+s.slice(2,0);//""
+//substring会取start和end中较小的值为start,二者相等返回空字符串，任何一个参数为负数被替换为0(即该值会成为start参数)
+var s="afghtee";
+s.substring(3,1)//"fg"
+s.substring(1,1)//""
+s.substring(3,-2)//"afg"
+//而substr比较特殊
+
+//substr的end参数表示，要截取的长度,若该参数为负数或0，都将返回空字符串
+var s="afghj";
+s.substr(2,0)//""
+m.substr(2,-3)//""
